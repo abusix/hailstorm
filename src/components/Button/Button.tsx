920c51465652ea900694c4f0f5c74493bd8addbb
david michel <michel.david@gmx.de>
2023-03-15 00:08:40 +0100

    initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const buttonVariants = {
+    primary:
+        'bg-primary-500 text-neutral-0 hover:bg-primary-600 active:bg-primary-600 focus:ring-2 focus:ring-primary-200 focus:bg-primary-600 disabled:bg-primary-200 fill-neutral-0',
+    secondary:
+        'text-neutral-700 bg-neutral-0 border border-neutral-400 hover:border-neutral-600 hover:text-neutral-800 active:bg-neutral-100 focus:ring-2 focus:ring-primary-200 focus:text-neutral-800 disabled:text-neutral-500 disabled:border-neutral-300 disabled:bg-neutral-0 fill-neutral-0',
+    minimal:
+        'text-neutral-700 hover:bg-neutral-100 hover:text-neutral-800 active:bg-neutral-200 focus:ring-2 focus:ring-primary-200 focus:text-neutral-800 disabled:text-neutral-500 disabled:bg-neutral-0 fill-neutral-0',
+    danger: 'text-neutral-0 bg-danger-500 hover:bg-danger-500 active:bg-danger-700 focus:ring-2 focus:ring-danger-100 focus:bg-danger-600 disabled:bg-danger-100 fill-neutral-0',
+    'danger-secondary':
+        'bg-neutral-0 text-danger-500 border border-danger-400 hover:bg-danger-50 hover:text-danger-600 active:border-danger-700 active:text-danger-700 active:bg-danger-100 focus:ring-2 focus:ring-danger-100 focus:text-danger-600 disabled:border-danger-100 disabled:text-danger-100 disabled:bg-neutral-0 fill-danger-600 disabled:fill-danger-100',
+};
+
+export type ButtonType = keyof typeof buttonVariants;
+
+const iconVariants = {
+    primary: 'text-neutral-0',
+    secondary:
+        'fill-neutral-600 group-hover:text-neutral-700 group-focus:text-neutral-700 group-disabled:text-neutral-400',
+    minimal:
+        'fill-neutral-600 group-hover:text-neutral-700 group-focus:text-neutral-700 group-disabled:text-neutral-400',
+    danger: '',
+    'danger-secondary': '',
+};
+
+export interface ButtonProps {
+    className?: string;
+    children: string;
+    type: ButtonType;
+    onClick: () => void;
+    loading?: boolean;
+    leftIcon?: any;
+    rightIcon?: any;
+    disabled?: boolean;
+}
+
+export default function Button({
+    className,
+    children,
+    onClick,
+    loading,
+    leftIcon,
+    rightIcon,
+    disabled = false,
+    type = 'primary',
+}: ButtonProps) {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {loading ? <p>Loading</p> : null}
+
+            {leftIcon && !loading ? (
+                <p>Icon Left Here</p>
+            ) : null}
+
+            {children}
+
+            {rightIcon ? (
+                <p>Icon Right Here</p>
+            ) : null}
+        </button>
+    );
+}
