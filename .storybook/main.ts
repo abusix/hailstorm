960a477cec0d6863dd70de14beff536eeaba58df
Rodrigo De la Rivera <crdelarivera@gmail.com>
2023-05-05 14:27:14 +0200

    refactor: updates dev packages and storybook
    Storybook config in TS + some extra options
    Storybook v7 + Vite builder
    Typescript v5
    
    update storybook with docs support

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(ts|tsx)"],
+  staticDirs: ["../assets"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      strictMode: true,
+    },
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,26 @@
+import type { Preview } from "@storybook/react";
+import "tailwindcss/tailwind.css";
+import "../src/index.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    layout: "centered", // "centered" | "fullscreen"
+    backgrounds: {
+      default: "white",
+      values: [
+        { name: "white", value: "#FFFFFF" },
+        { name: "light", value: "#FAFAFC" },
+        { name: "dark", value: "#212121" },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
