f594849696ed63a6f0052b6c1131842d20767f92
mnlfischer <mnlfischer@users.noreply.github.com>
2023-05-09 08:36:52 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@e3ba41cfaeab686dc629bd4d8fb9847172327325 🚀

---
+++ assets/menu.stories-4e3fc0a4.js
@@ -0,0 +1,14 @@
+import{a as n,j as r}from"./jsx-runtime-a888423b.js";import{a as e}from"./menu-2ff7e251.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./menu-info-item-cb36c8ba.js";import"./class-names-2bb0c168.js";import"./keyboard-f3ae5531.js";import"./bugs-6ad166a8.js";import"./use-tracked-pointer-b0b2487f.js";import"./focus-management-8ba95dc1.js";import"./open-closed-818d3463.js";import"./use-resolve-button-type-3a664423.js";import"./use-owner-3a57d220.js";import"./menu-separator-cd16a535.js";import"./menu-title-c9b1e564.js";const j={title:"Menu",component:e},t={render:()=>n("div",{className:"w-52",children:r(e,{children:[n(e.Button,{children:"Open Menu"}),r(e.Items,{children:[n(e.Title,{children:"TITLE"}),n(e.Item,{disabled:!1,children:"Label"}),n(e.Item,{children:"Label"}),n(e.Item,{children:"Label"})]})]})})};var m,a,i;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <div className="w-52">
+      <Menu>
+        <Menu.Button>Open Menu</Menu.Button>
+        <Menu.Items>
+          <Menu.Title>TITLE</Menu.Title>
+          <Menu.Item disabled={false}>Label</Menu.Item>
+          <Menu.Item>Label</Menu.Item>
+          <Menu.Item>Label</Menu.Item>
+        </Menu.Items>
+      </Menu>
+    </div>
+}`,...(i=(a=t.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const v=["Default"];export{t as Default,v as __namedExportsOrder,j as default};
+//# sourceMappingURL=menu.stories-4e3fc0a4.js.map
