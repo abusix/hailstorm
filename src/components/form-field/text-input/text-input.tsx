a6e5615f18661da65ebf6d8c8cb94076ba5b63f1
Joseph Ribbe <joseph.ribbe@abusix.com>
2023-05-22 08:50:00 +0200

    Added changes for text input type

--- src/components/form-field/text-input/text-input.tsx
+++ src/components/form-field/text-input/text-input.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { useRef } from "react";
-import classNames from "../../../util/class-names";
+import React, { useRef } from 'react';
+import classNames from '../../../util/class-names';
 
 export interface TextInputProps {
   id: string;
@@ -8,7 +7,7 @@ export interface TextInputProps {
   value: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   readOnly?: boolean;
-  type?: "text" | "password";
+  type?: 'text' | 'password' | 'email';
   autoSelect?: boolean;
   ariaDescribedBy?: string;
   LeftIcon?: React.ElementType;
@@ -22,7 +21,7 @@ export const TextInput = ({
   value,
   onChange,
   ariaDescribedBy,
-  type = "text",
+  type = 'text',
   LeftIcon,
   readOnly,
   autoSelect,
@@ -30,13 +29,13 @@ export const TextInput = ({
   disabled,
 }: TextInputProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
-
+  
   function handleAutoSelection() {
     if (autoSelect && inputRef.current) {
       inputRef.current.select();
     }
   }
-
+  
   return (
     <div className="relative w-full">
       {LeftIcon ? (
@@ -44,10 +43,10 @@ export const TextInput = ({
           className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
           aria-hidden="true"
         >
-          <LeftIcon className="text-gray-400 h-4 w-4 fill-neutral-600" />
+          <LeftIcon className="text-gray-400 h-4 w-4 fill-neutral-600"/>
         </div>
       ) : null}
-
+      
       <input
         ref={inputRef}
         id={id}
@@ -62,14 +61,14 @@ export const TextInput = ({
         onClick={autoSelect ? handleAutoSelection : undefined}
         type={type}
         className={classNames(
-          "paragraph-100 block w-full rounded border border-neutral-400 py-2 pl-3 pr-2 text-neutral-800 placeholder:text-neutral-600 focus:outline-none",
-          LeftIcon && "pl-9",
-          readOnly && "bg-neutral-100",
-          disabled && "cursor-not-allowed bg-neutral-100 text-neutral-600",
+          'paragraph-100 block w-full rounded border border-neutral-400 py-2 pl-3 pr-2 text-neutral-800 placeholder:text-neutral-600 focus:outline-none',
+          LeftIcon && 'pl-9',
+          readOnly && 'bg-neutral-100',
+          disabled && 'cursor-not-allowed bg-neutral-100 text-neutral-600',
           !error &&
-            !disabled &&
-            "hover:border-neutral-600 focus:border-primary-400 focus:ring-2 focus:ring-primary-200",
-          error && !disabled && "border-danger-500"
+          !disabled &&
+          'hover:border-neutral-600 focus:border-primary-400 focus:ring-2 focus:ring-primary-200',
+          error && !disabled && 'border-danger-500'
         )}
         disabled={disabled}
       />
