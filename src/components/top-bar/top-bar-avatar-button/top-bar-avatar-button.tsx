df6390122bed1164a775c94aaf9b90562b2d6b8b
Joseph Ribbe <joseph.ribbe@abusix.com>
2023-06-05 14:49:49 +0200

    fixed menu avatar’s chevron icon size problem

--- src/components/top-bar/top-bar-avatar-button/top-bar-avatar-button.tsx
+++ src/components/top-bar/top-bar-avatar-button/top-bar-avatar-button.tsx
@@ -11,7 +11,7 @@ export const TopBarAvatarButton = ({ children }: TopBarAvatarButtonProps) => {
   return (
     <Menu.Button className="flex items-center gap-1 rounded px-1 py-1 hover:bg-primary-900 focus:outline-none focus:ring-2 focus:ring-primary-200">
       <Avatar color="primary">{children}</Avatar>
-      <ChevronDownIcon className="fill-neutral-400" />
+      <ChevronDownIcon className="h-4 w-4 fill-neutral-400" />
     </Menu.Button>
   );
 };
