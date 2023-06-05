0485d7cfa18ae006ff6172f3ec896d4a92eb5fd3
Manuel <manuel.fischer@abusix.com>
2023-06-05 13:18:53 +0200

    remove nested example

--- src/components/tab/tab.stories.tsx
+++ src/components/tab/tab.stories.tsx
@@ -20,18 +20,7 @@ export const Primary: Story = {
         <Tab.Button onClick={() => null}>Tab 3</Tab.Button>
       </Tab.List>
       <Tab.Panels>
-        <Tab.Panel>
-          <Tab.List>
-            <Tab.Button onClick={() => null}>Tab 1.1</Tab.Button>
-            <Tab.Button onClick={() => null}>Tab 1.2</Tab.Button>
-            <Tab.Button onClick={() => null}>Tab 1.3</Tab.Button>
-          </Tab.List>
-          <Tab.Panels>
-            <Tab.Panel>Content 1.1</Tab.Panel>
-            <Tab.Panel>Content 1.2</Tab.Panel>
-            <Tab.Panel>Content 1.3</Tab.Panel>
-          </Tab.Panels>
-        </Tab.Panel>
+        <Tab.Panel>Content 1</Tab.Panel>
         <Tab.Panel>Content 2</Tab.Panel>
         <Tab.Panel>Content 3</Tab.Panel>
       </Tab.Panels>
