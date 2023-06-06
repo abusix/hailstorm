e83803374dc77ce66e0cbd5f45c81cbbc8049495
Joseph Ribbe <joseph.ribbe@abusix.com>
2023-06-06 10:38:42 +0200

    added nested tabs example

--- src/components/tab/tab.stories.tsx
+++ src/components/tab/tab.stories.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import type { Meta, StoryObj } from "@storybook/react";
-import { Tab } from "./tab";
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Tab } from './tab';
+import { TabType } from './tab-context';
 
 const meta: Meta<typeof Tab> = {
-  title: "Tab",
+  title: 'Tab',
   component: Tab,
 };
 
@@ -11,7 +12,51 @@ export default meta;
 
 type Story = StoryObj<typeof Tab>;
 
+const TabsExample = ({ type = 'primary' }: { type?: TabType }) => {
+  return (
+    <Tab type={type}>
+      <Tab.List>
+        <Tab.Button onClick={() => null}>Tab 1</Tab.Button>
+        <Tab.Button onClick={() => null}>Tab 2</Tab.Button>
+        <Tab.Button onClick={() => null}>Tab 3</Tab.Button>
+      </Tab.List>
+      
+      <Tab.Panels>
+        <Tab.Panel>
+          <p className={'p-4 paragraph-200'}>
+            Content 1
+          </p>
+        </Tab.Panel>
+        
+        <Tab.Panel>
+          <p className={'p-4 paragraph-200'}>
+            Content 2
+          </p>
+        </Tab.Panel>
+        
+        <Tab.Panel>
+          <p className={'p-4 paragraph-200'}>
+            Content 3
+          </p>
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab>
+  )
+}
+
 export const Primary: Story = {
+  render: () => (
+    <TabsExample/>
+  ),
+};
+
+export const Seconday: Story = {
+  render: () => (
+    <TabsExample type="secondary"/>
+  ),
+};
+
+export const NestedTabs: Story = {
   render: () => (
     <Tab>
       <Tab.List>
@@ -19,27 +64,25 @@ export const Primary: Story = {
         <Tab.Button onClick={() => null}>Tab 2</Tab.Button>
         <Tab.Button onClick={() => null}>Tab 3</Tab.Button>
       </Tab.List>
+      
       <Tab.Panels>
-        <Tab.Panel>Content 1</Tab.Panel>
-        <Tab.Panel>Content 2</Tab.Panel>
-        <Tab.Panel>Content 3</Tab.Panel>
-      </Tab.Panels>
-    </Tab>
-  ),
-};
-
-export const Seconday: Story = {
-  render: () => (
-    <Tab type="secondary">
-      <Tab.List>
-        <Tab.Button onClick={() => null}>Tab 1</Tab.Button>
-        <Tab.Button onClick={() => null}>Tab 2</Tab.Button>
-        <Tab.Button onClick={() => null}>Tab 3</Tab.Button>
-      </Tab.List>
-      <Tab.Panels>
-        <Tab.Panel>Content 1</Tab.Panel>
-        <Tab.Panel>Content 2</Tab.Panel>
-        <Tab.Panel>Content 3</Tab.Panel>
+        <Tab.Panel>
+          <div className="p-2">
+            <TabsExample/>
+          </div>
+        </Tab.Panel>
+        
+        <Tab.Panel>
+          <div className="p-2">
+            <TabsExample/>
+          </div>
+        </Tab.Panel>
+        
+        <Tab.Panel>
+          <div className="p-2">
+            <TabsExample/>
+          </div>
+        </Tab.Panel>
       </Tab.Panels>
     </Tab>
   ),
