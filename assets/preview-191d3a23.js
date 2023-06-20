e2106c1b56634e4d5f2e77ae45ea6c70106f4541
Coderwelsch <4981006+Coderwelsch@users.noreply.github.com>
2023-06-20 13:12:45 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@4d49fecb34164d74f231907999fa70f22b108363 🚀

--- assets/preview-191d3a23.js
+++ assets/preview-191d3a23.js
@@ -1,2 +1,2 @@
 const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},layout:"centered",backgrounds:{default:"white",values:[{name:"white",value:"#FFFFFF"},{name:"light",value:"#FAFAFC"},{name:"dark",value:"#212121"}]},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-90f87a82.js.map
+//# sourceMappingURL=preview-191d3a23.js.map
