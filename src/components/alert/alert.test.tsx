6f4d5c3f3ecf36ca6190d67ec2b60bb737bd7ce4
Manuel <manuel.fischer@abusix.com>
2023-09-22 11:50:35 +0200

    fix(test): use screen

--- src/components/alert/alert.test.tsx
+++ src/components/alert/alert.test.tsx
@@ -1,19 +1,19 @@
 import { describe, expect, it } from "vitest";
-import { getByText, render } from "@testing-library/react";
+import { getByText, render, screen } from "@testing-library/react";
 import React from "react";
 import { Alert } from "./alert";
 
 describe("Alert", () => {
     it("renders an alert with a title and children", () => {
         // ARRANGE
-        const { getByRole } = render(
+        render(
             <Alert intent="info" title="Hello">
                 World
             </Alert>
         );
 
         // ASSERT
-        const alert = getByRole("alert");
+        const alert = screen.getByRole("alert");
         expect(alert).toBeInTheDocument();
         expect(getByText(alert, "Hello")).toBeInTheDocument();
         expect(getByText(alert, "World")).toBeInTheDocument();
