ab4c4285bae59aca4d400d126956eca36009d8d9
Manuel <manuel.fischer@abusix.com>
2023-09-28 13:02:34 +0200

    feat(components): add className prop to sidebar container

--- src/components/sidebar-container/sidebar-container.tsx
+++ src/components/sidebar-container/sidebar-container.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { classNames } from "../../util/class-names";
 
 interface SidebarContainerProps {
     sidebarContent: React.ReactNode;
     pageContent: React.ReactNode;
+    className?: string;
 }
 
-export const SidebarContainer = ({ sidebarContent, pageContent }: SidebarContainerProps) => {
+export const SidebarContainer = ({
+    sidebarContent,
+    pageContent,
+    className,
+}: SidebarContainerProps) => {
     return (
-        <div className="flex flex-grow">
+        <div className={classNames("flex flex-grow", className)}>
             {sidebarContent}
             <div className="w-full overflow-x-hidden bg-neutral-50">
                 <div className="mx-auto w-full max-w-285 px-10 py-5">{pageContent}</div>
