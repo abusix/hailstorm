aab2208fbe17b3ad9a9eea4ad828fd681c88d4b8
Manuel <manuel.fischer@abusix.com>
2023-09-28 13:07:41 +0200

    feat(components): add className prop to spinner overlay

--- src/components/spinner-overlay/spinner-overlay.tsx
+++ src/components/spinner-overlay/spinner-overlay.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Spinner } from "../spinner/spinner";
+import { classNames } from "../../util/class-names";
 
-export const SpinnerOverlay = () => {
+export interface SpinnerOverlayProps {
+    className?: string;
+}
+
+export const SpinnerOverlay = ({ className }: SpinnerOverlayProps) => {
     return (
-        <div className="absolute bottom-0 left-0 right-0 top-0 z-10 flex items-center justify-center bg-neutral-0 opacity-50">
+        <div
+            className={classNames(
+                "absolute bottom-0 left-0 right-0 top-0 z-10 flex items-center justify-center bg-neutral-0 opacity-50",
+                className
+            )}
+        >
             <Spinner size="medium" />
         </div>
     );
