e64361f62cb88af22042f806e020a3c6b1cb9f64
pallendes <allendes91@gmail.com>
2023-10-17 08:35:33 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@657f9a870ff7bd72fb00385b6c9cf75afd7247c8 🚀

---
+++ assets/skeleton.stories-03486e6c.js
@@ -0,0 +1,8 @@
+import{j as o}from"./jsx-runtime-94f6e698.js";import{c as m}from"./class-names-9cb99c69.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const s=({className:e,isAnimated:l=!1})=>o.jsx("span",{className:m("block rounded-sm bg-neutral-100",l&&"animate-pulse",e)});try{s.displayName="Skeleton",s.__docgenInfo={description:"",displayName:"Skeleton",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},isAnimated:{defaultValue:{value:"false"},description:"",name:"isAnimated",required:!1,type:{name:"boolean"}}}}}catch{}const u={title:"Skeleton",component:s},a={render:e=>o.jsx(s,{...e}),args:{className:"w-96 h-8",isAnimated:!1}};var r,t,n;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <Skeleton {...args} />,
+  args: {
+    className: "w-96 h-8",
+    isAnimated: false
+  }
+}`,...(n=(t=a.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const f=["Default"];export{a as Default,f as __namedExportsOrder,u as default};
+//# sourceMappingURL=skeleton.stories-03486e6c.js.map
