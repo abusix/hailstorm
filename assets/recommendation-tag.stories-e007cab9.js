cd864b06126bb0ed6ddbc99ea457af49f9910052
mnlfischer <mnlfischer@users.noreply.github.com>
2023-11-21 08:41:59 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@62e6afab60158df92542842c9c6818faa4bb5133 🚀

---
+++ assets/recommendation-tag.stories-e007cab9.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-94f6e698.js";import{r as u}from"./index-8db94870.js";import{R as n,F as o}from"./form-field-e320f45b.js";import{P as R}from"./panel-a25594bd.js";import"./_commonjsHelpers-042e6b4d.js";import"./zoom-to-fit-icon-51e20867.js";import"./class-names-9cb99c69.js";import"./keyboard-5e933fe8.js";import"./focus-management-d35c5d70.js";import"./use-flags-cf9437e9.js";import"./label-03e90ffb.js";import"./description-e80611a4.js";import"./use-text-value-1d5c8b57.js";import"./hidden-c4917ca2.js";import"./bugs-7a186658.js";import"./open-closed-535072ce.js";import"./use-resolve-button-type-68f089c3.js";import"./badge-d7546b2f.js";import"./tag-090442e4.js";const b={title:"Input/RecommendationTag",component:n},h=()=>{const[c,l]=u.useState("value_1");return e.jsx("div",{className:"w-96",children:e.jsx(o,{children:e.jsxs(o.RadioBox,{value:c,onChange:x=>{l(x)},id:"value",children:[e.jsxs(o.RadioBox.Option,{value:"option_1",children:["Option 1",e.jsx(n,{children:"Recommended!"})]}),e.jsx(o.RadioBox.Option,{value:"option_2",children:"Option 2"}),e.jsx(o.RadioBox.Option,{value:"option_3",children:"Option 3"})]})})})},r={render:()=>e.jsx(h,{})},a={render:()=>e.jsx("div",{className:"w-96",children:e.jsxs(R,{className:"relative",children:[e.jsx("p",{children:"This example uses a Panel component"}),e.jsx(n,{children:"Recommended!"})]})})};var t,i,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => {
+    return <RadioBoxWithRecommendationTag />;
+  }
+}`,...(s=(i=r.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var m,p,d;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <div className="w-96">
+            <Panel className="relative">
+                <p>This example uses a Panel component</p>
+                <RecommendationTag>Recommended!</RecommendationTag>
+            </Panel>
+        </div>
+}`,...(d=(p=a.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const k=["Default","PanelExample"];export{r as Default,a as PanelExample,k as __namedExportsOrder,b as default};
+//# sourceMappingURL=recommendation-tag.stories-e007cab9.js.map
