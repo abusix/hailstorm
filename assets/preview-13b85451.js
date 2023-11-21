9156055443739d053b959ebf1c50fa924244786a
mnlfischer <mnlfischer@users.noreply.github.com>
2023-11-21 07:51:50 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@b1e5a2c91adea1b5008fca9ab974313da943c594 🚀

--- assets/preview-13b85451.js
+++ assets/preview-13b85451.js
@@ -1,2 +1,2 @@
 const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},layout:"centered",backgrounds:{default:"white",values:[{name:"white",value:"#FFFFFF"},{name:"light",value:"#FAFAFC"},{name:"dark",value:"#212121"}]},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Home","Icons gallery"]}}}};export{e as default};
-//# sourceMappingURL=preview-aa7876ad.js.map
+//# sourceMappingURL=preview-13b85451.js.map
