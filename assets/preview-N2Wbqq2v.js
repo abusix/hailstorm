f2f0db85c45ce96733e1bd307e0f7efe96403d26
pallendes <allendes91@gmail.com>
2023-12-18 07:13:49 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@055f77a07ea1eb808d8241faf26bfc6263bbe4cd 🚀

--- assets/preview-N2Wbqq2v.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-f1qe9t8V.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-J9ykBsFD.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-J9ykBsFD.js","./iframe-f1qe9t8V.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-ZbrxiSSw.js","./index-ogXoivrg.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
