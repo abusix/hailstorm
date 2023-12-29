2ff4d47ec05e224954ada7e276883719a91fdd10
pallendes <allendes91@gmail.com>
2023-12-29 11:54:46 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@f558bb720df18ad53a9e0abfe8d86edbcf757e1d 🚀

---
+++ assets/panel.stories-PeKHbEW-.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{P as l}from"./panel-4C-usJgI.js";import{B as e}from"./button-aWfy49w9.js";import{g as d}from"./storybook-utils-94J-CLQK.js";import{T as u}from"./toggle-xpxWAZQy.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./class-names-J070TWId.js";import"./spinner-E2xQXJan.js";import"./render-Tie-ixVq.js";import"./keyboard-WH7JXezp.js";import"./bugs-DTVvle51.js";import"./label-B5aT1uE9.js";import"./description-pJu2sNYX.js";import"./use-resolve-button-type-9qTOIf0P.js";import"./hidden-T1ZmySp8.js";import"./use-disposables-w3ythNQc.js";const L={title:"Panel",parameters:{...d("Simple container used to group and organize elements in the UI."),backgrounds:{default:"light"}},component:l,args:{className:"",children:"Panel with text content"}},o={},n=()=>{},r={args:{children:t.jsxs(t.Fragment,{children:[t.jsx(e,{variant:"primary",onClick:n,children:"Button A"}),t.jsx(u,{checked:!0,ariaLabel:"test",onChange:n}),t.jsx(e,{variant:"secondary",onClick:n,children:"Button B"}),t.jsx("p",{children:" Paragraph content"})]})}};var a,s,i;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(i=(s=o.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var p,m,c;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: <>
+                <Button variant="primary" onClick={noop}>
+                    Button A
+                </Button>
+                <Toggle checked ariaLabel="test" onChange={noop} />
+                <Button variant="secondary" onClick={noop}>
+                    Button B
+                </Button>
+                <p> Paragraph content</p>
+            </>
+  }
+}`,...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const W=["Default","WithComponents"];export{o as Default,r as WithComponents,W as __namedExportsOrder,L as default};
---
+++ assets/toggle.stories-f0JSWT56.js
@@ -0,0 +1,7 @@
+import{j as n}from"./jsx-runtime-vNq4Oc-g.js";import{R as s}from"./index-4g5l5LRQ.js";import{T as c}from"./toggle-xpxWAZQy.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./class-names-J070TWId.js";import"./render-Tie-ixVq.js";import"./keyboard-WH7JXezp.js";import"./bugs-DTVvle51.js";import"./label-B5aT1uE9.js";import"./description-pJu2sNYX.js";import"./use-resolve-button-type-9qTOIf0P.js";import"./hidden-T1ZmySp8.js";import"./use-disposables-w3ythNQc.js";const D={title:"Toggle",component:c},l=({checked:e,disabled:p})=>{const[r,t]=s.useState(!1);return s.useEffect(()=>{t(e)},[e]),n.jsx(c,{checked:r,disabled:p,onChange:()=>t(!r),ariaLabel:"Enable notifications"})},o={render:e=>n.jsx(l,{...e}),args:{checked:!1,disabled:!1}};var a,i,m;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => <ToggleWithHook {...args} />,
+  args: {
+    checked: false,
+    disabled: false
+  }
+}`,...(m=(i=o.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const H=["Default"];export{o as Default,H as __namedExportsOrder,D as default};
