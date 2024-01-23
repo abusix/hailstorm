a557a9de37b8d764f122e3ea690459a6212de203
Manuel <manuel.fischer@abusix.com>
2024-01-23 13:49:15 +0100

    BREAKING CHANGE(components): use switch group to support switch label

--- src/components/toggle/toggle.tsx
+++ src/components/toggle/toggle.tsx
@@ -2,23 +2,39 @@ import React from "react";
 import { Switch as HeadlessSwitch } from "@headlessui/react";
 import { classNames } from "../../util/class-names";
 
-export interface ToggleProps {
+interface LabelProps {
+    children: React.ReactNode;
+    passive?: boolean;
+}
+
+const Label = ({ children, passive = false }: LabelProps) => {
+    return (
+        <HeadlessSwitch.Label
+            className={classNames(
+                "paragraph-200 cursor-pointer text-neutral-800 ui-disabled:text-danger-600"
+            )}
+            passive={passive}
+        >
+            {children}
+        </HeadlessSwitch.Label>
+    );
+};
+
+export interface SwitchProps {
     checked: boolean;
     onChange: () => void;
     disabled?: boolean;
     ariaLabel: string;
 }
 
-export const Toggle = ({ checked = false, onChange, disabled = false, ariaLabel }: ToggleProps) => {
+const Switch = ({ checked = false, onChange, disabled = false, ariaLabel }: SwitchProps) => {
     return (
         <HeadlessSwitch
             checked={checked}
             disabled={disabled}
             onChange={onChange}
             className={classNames(
-                "relative inline-flex h-4 w-7 items-center rounded-full",
-                !checked && "bg-neutral-400 hover:bg-neutral-500 disabled:bg-neutral-100",
-                checked && "bg-primary-500 hover:bg-primary-600 disabled:bg-neutral-100"
+                "relative inline-flex h-4 w-7 items-center rounded-full ui-checked:bg-primary-500 ui-checked:hover:bg-primary-600 ui-checked:disabled:bg-neutral-100 ui-not-checked:bg-neutral-400 ui-not-checked:hover:bg-neutral-500 ui-not-checked:disabled:bg-neutral-100"
             )}
         >
             <span className="sr-only">{ariaLabel}</span>
@@ -35,3 +51,20 @@ export const Toggle = ({ checked = false, onChange, disabled = false, ariaLabel 
         </HeadlessSwitch>
     );
 };
+
+export interface ToggleProps {
+    children?: React.ReactNode;
+}
+
+const Toggle = ({ children }: ToggleProps) => {
+    return (
+        <HeadlessSwitch.Group>
+            <div className="flex items-center gap-3">{children}</div>
+        </HeadlessSwitch.Group>
+    );
+};
+
+Toggle.Switch = Switch;
+Toggle.Label = Label;
+
+export { Toggle };
