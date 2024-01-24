706c3d3ffaf5531be05924739ef806e76a8391ec
mnlfischer <mnlfischer@users.noreply.github.com>
2024-01-24 09:45:53 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@297357d377aa3f2498f4f55c2ad45ecd4f208bc1 🚀

---
+++ assets/recommendation-tag.stories-L4wIIBZi.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{r as u}from"./index-4g5l5LRQ.js";import{b as t,a as o}from"./form-field-JXOcAPS8.js";import{P as h}from"./panel-4C-usJgI.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./zoom-to-fit-icon---bq2i_K.js";import"./class-names-J070TWId.js";import"./render-w54E0F54.js";import"./keyboard-o6QkQYrw.js";import"./focus-management-gzNHToo7.js";import"./disposables-x8EK1kJp.js";import"./owner-fKE-7Rwj.js";import"./use-flags-t0jpIEJ-.js";import"./use-is-mounted-2jl3pcgu.js";import"./label-4h3njXKv.js";import"./description-5F8ZuhcY.js";import"./use-text-value-q5k1B-6y.js";import"./hidden-qqzWvBMn.js";import"./bugs-DTVvle51.js";import"./use-disposables-9CA2w67p.js";import"./open-closed--zB8LTaK.js";import"./use-resolve-button-type-_yXBvNtz.js";import"./use-owner-g5X0ZmiI.js";import"./badge-oLUvZuwe.js";import"./platform-xf9k1Dsu.js";import"./tag-IvA-YcUf.js";const H={title:"Input/RecommendationTag",component:t},R=()=>{const[c,l]=u.useState("value_1");return e.jsx("div",{className:"w-96",children:e.jsx(o,{children:e.jsxs(o.RadioBox,{value:c,onChange:x=>{l(x)},id:"value",children:[e.jsxs(o.RadioBox.Option,{value:"option_1",children:["Option 1",e.jsx(t,{children:"Recommended!"})]}),e.jsx(o.RadioBox.Option,{value:"option_2",children:"Option 2"}),e.jsx(o.RadioBox.Option,{value:"option_3",children:"Option 3"})]})})})},r={render:()=>e.jsx(R,{})},a={render:()=>e.jsx("div",{className:"w-96",children:e.jsxs(h,{className:"relative",children:[e.jsx("p",{children:"This example uses a Panel component"}),e.jsx(t,{children:"Recommended!"})]})})};var n,i,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => {
+    return <RadioBoxWithRecommendationTag />;
+  }
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var s,p,d;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <div className="w-96">
+            <Panel className="relative">
+                <p>This example uses a Panel component</p>
+                <RecommendationTag>Recommended!</RecommendationTag>
+            </Panel>
+        </div>
+}`,...(d=(p=a.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const J=["Default","PanelExample"];export{r as Default,a as PanelExample,J as __namedExportsOrder,H as default};
