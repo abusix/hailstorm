0e0b76e9077ae95c990bfa90ae4499183e5535c3
coderwelsch <joseph.ribbe@abusix.com>
2024-03-12 13:29:13 +0100

    renamed active to isActive

--- src/components/breadcrumbs/breadcrumbs-item.tsx
+++ src/components/breadcrumbs/breadcrumbs-item.tsx
@@ -5,21 +5,21 @@ interface BreadcrumbItemProps {
     children: React.ReactNode;
     className?: string;
     href?: string;
-    active?: boolean;
+    isActive?: boolean;
 }
 
 export const BreadcrumbsItem: React.FC<BreadcrumbItemProps> = ({
     children,
     className,
     href,
-    active,
+    isActive,
 }) => {
     return (
         <a
             className={classNames(
                 "headline-500 text-neutral-800",
-                !active && "cursor-pointer underline-offset-2 hover:underline",
-                active && "text-black",
+                !isActive && "cursor-pointer underline-offset-2 hover:underline",
+                isActive && "text-black",
                 className
             )}
             href={href}
