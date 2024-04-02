13c35aeaa3ac8c44d8b993a80167ed4366de5849
coderwelsch <joseph.ribbe@abusix.com>
2024-04-02 13:30:46 +0200

    feat: added bg-primary-100 to active tab buttons

--- src/components/tab/tab-button.tsx
+++ src/components/tab/tab-button.tsx
@@ -13,7 +13,7 @@ const buttonVariants: Record<TabType, string> = {
     primary:
         "relative whitespace-nowrap focus:ring-2 ring:primary-200 ui-not-selected:hover:text-neutral-800 ui-not-selected:after:hidden ui-selected:after:absolute ui-selected:after:left-0 ui-selected:after:right-0 ui-selected:after:-bottom-2 ui-selected:after:block ui-selected:after:h-0.5 ui-selected:after:bg-primary-500 disabled:ui-selected:after:bg-primary-500 disabled:text-neutral-500",
     secondary:
-        "px-4 py-2 ui-not-selected:bg-transparent ui-not-selected:hover:text-neutral-800 focus:ring-2 ring:primary-200 ui-not-selected:hover:bg-neutral-100 ui-selected:text-primary-500 ui-selected:bg-primary-50 disabled:text-neutral-500",
+        "px-4 py-2 ui-not-selected:bg-transparent ui-not-selected:hover:text-neutral-800 focus:ring-2 ring:primary-200 ui-not-selected:hover:bg-neutral-100 ui-selected:text-primary-500 ui-selected:bg-primary-100 disabled:text-neutral-500",
 };
 
 export const TabButton = <TTag extends React.ElementType>({
