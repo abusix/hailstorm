4b493069db5ba9f7d140b8874e18dbe226bc3ddd
Manuel <manuel.fischer@abusix.com>
2024-06-19 11:53:58 +0200

    fix(dependencies): update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
     stories: ["../src/**/*.stories.@(ts|tsx)", "../src/**/*.@(mdx|stories.@(md))"],
     staticDirs: ["../assets"],
+
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
@@ -10,17 +11,22 @@ const config: StorybookConfig = {
         "@storybook/addon-docs",
         "@storybook/addon-mdx-gfm",
     ],
+
     framework: {
         name: "@storybook/react-vite",
         options: {
             strictMode: true,
         },
     },
-    docs: {
-        autodocs: true,
-    },
+
+    docs: {},
+
     core: {
         disableTelemetry: true,
     },
+
+    typescript: {
+        reactDocgen: "react-docgen-typescript"
+    }
 };
 export default config;
