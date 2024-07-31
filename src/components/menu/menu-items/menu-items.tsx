626d1bf059529c1622ef41e711da876d0bb3c2ab
Manuel <manuel.fischer@abusix.com>
2024-07-31 13:48:39 +0200

    refactor: Update menu items component with @headlessui/react v2.0 changes

--- src/components/menu/menu-items/menu-items.tsx
+++ src/components/menu/menu-items/menu-items.tsx
@@ -1,13 +1,13 @@
+import { MenuItems as HeadlessUiMenuItems } from "@headlessui/react";
 import React from "react";
-import { Menu as HeadlessMenu } from "@headlessui/react";
 
 export interface MenuItemsProps {
     children: React.ReactNode;
 }
 export const MenuItems = ({ children }: MenuItemsProps) => {
     return (
-        <HeadlessMenu.Items className="absolute right-0 z-10 mt-2 flex w-52 flex-col rounded-md bg-neutral-0 py-2 shadow-md outline-none">
+        <HeadlessUiMenuItems className="absolute right-0 z-10 mt-2 flex w-52 flex-col rounded-md bg-neutral-0 py-2 shadow-md outline-none">
             {children}
-        </HeadlessMenu.Items>
+        </HeadlessUiMenuItems>
     );
 };
