48e18007b4188fa129997fd607d0b5da86215783
Manuel <manuel.fischer@abusix.com>
2024-07-31 13:51:09 +0200

    refactor: Update NavigationPopoverButton to use @headlessui/react v2.0 changes

--- src/components/navigation/navigation-popover-button.tsx
+++ src/components/navigation/navigation-popover-button.tsx
@@ -1,4 +1,4 @@
-import { Popover } from "@headlessui/react";
+import { PopoverButton as HeadlessUiPopoverButton } from "@headlessui/react";
 import React from "react";
 import { useNavigationPopoverContext } from "./navigation-popover-context";
 
@@ -18,7 +18,7 @@ export const NavigationPopoverButton = ({
     } = useNavigationPopoverContext();
 
     return (
-        <Popover.Button
+        <HeadlessUiPopoverButton
             ref={(el) => el && setReferenceElement(el)}
             className="flex w-full cursor-pointer items-center gap-x-3 px-4 py-3 text-left text-sm text-neutral-0 hover:bg-primary-900+10 ui-open:bg-primary-900+8 ui-open:font-semibold"
             onClick={onClick}
@@ -27,6 +27,6 @@ export const NavigationPopoverButton = ({
                 {LeftIcon && <LeftIcon className="h-4 w-4" />}
                 {children}
             </>
-        </Popover.Button>
+        </HeadlessUiPopoverButton>
     );
 };
