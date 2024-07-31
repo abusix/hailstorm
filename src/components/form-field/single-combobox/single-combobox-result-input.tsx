338a99e22c8e9dcae4dc151c0644446e5996b879
Manuel <manuel.fischer@abusix.com>
2024-07-31 13:35:57 +0200

    refactor: Update SingleComboboxResultInput component with @headlessui/react v2.0 changes

--- src/components/form-field/single-combobox/single-combobox-result-input.tsx
+++ src/components/form-field/single-combobox/single-combobox-result-input.tsx
@@ -1,7 +1,7 @@
-import { Combobox as HeadlessCombobox } from "@headlessui/react";
+import { ComboboxButton as HeadlessUiComboboxButton } from "@headlessui/react";
 import React from "react";
-import { Tag } from "../../tag/tag";
 import { CaretDownIcon } from "../../../icons";
+import { Tag } from "../../tag/tag";
 
 export interface SingleComboboxResultInputProps {
     onUnselect: () => void;
@@ -16,11 +16,11 @@ export const SingleComboboxResultInput = ({
         <div className="relative">
             <div className="paragraph-100 flex h-8 w-full items-center rounded border border-neutral-400 py-2 pl-3 pr-8 focus-visible:border-primary-400 focus-visible:ring-2 focus-visible:ring-primary-200">
                 <Tag onClick={onUnselect}>{children}</Tag>
-                <HeadlessCombobox.Button className="absolute inset-y-0 right-0 flex items-center px-1.5">
+                <HeadlessUiComboboxButton className="absolute inset-y-0 right-0 flex items-center px-1.5">
                     <div className="flex h-5 w-5 items-center justify-center rounded rounded-r-md bg-neutral-100">
                         <CaretDownIcon className="h-3 w-3 fill-neutral-600" aria-hidden="true" />
                     </div>
-                </HeadlessCombobox.Button>
+                </HeadlessUiComboboxButton>
             </div>
         </div>
     );
