d1aaa27ba7457f9cf5c8605bc601e81051915bec
Manuel <manuel.fischer@abusix.com>
2024-07-31 13:54:26 +0200

    refactor: Update SidesheetPanelHeaderTitle to use @headlessui/react v2.0 changes

--- src/components/sidesheet/sidesheet-panel-header-title.tsx
+++ src/components/sidesheet/sidesheet-panel-header-title.tsx
@@ -1,10 +1,10 @@
+import { DialogTitle as HeadlessUiDialogTitle } from "@headlessui/react";
 import React from "react";
-import { Dialog as HeadlessDialog } from "@headlessui/react";
 
 export interface PanelHeaderTitleProps {
     children: React.ReactNode;
 }
 
 export const SidesheetPanelHeaderTitle = ({ children }: PanelHeaderTitleProps) => {
-    return <HeadlessDialog.Title className="headline-600">{children}</HeadlessDialog.Title>;
+    return <HeadlessUiDialogTitle className="headline-600">{children}</HeadlessUiDialogTitle>;
 };
