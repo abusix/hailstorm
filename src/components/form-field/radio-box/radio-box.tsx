031cade8a38d2823f7f3a42343f768c4d1f13d4c
Manuel <manuel.fischer@abusix.com>
2024-07-31 13:15:34 +0200

    refactor: Update RadioBox component with @headlessui/react v2.0 changes

--- src/components/form-field/radio-box/radio-box.tsx
+++ src/components/form-field/radio-box/radio-box.tsx
@@ -1,5 +1,5 @@
+import { RadioGroup as HeadlessUiRadioGroup } from "@headlessui/react";
 import React from "react";
-import { RadioGroup } from "@headlessui/react";
 import { RadioBoxOption } from "./radio-box-option";
 
 export interface RadioBoxProps {
@@ -12,9 +12,9 @@ export interface RadioBoxProps {
 
 export const RadioBox = ({ id, value, children, onChange, className }: RadioBoxProps) => {
     return (
-        <RadioGroup id={id} value={value} onChange={onChange} className={className}>
+        <HeadlessUiRadioGroup id={id} value={value} onChange={onChange} className={className}>
             {children}
-        </RadioGroup>
+        </HeadlessUiRadioGroup>
     );
 };
 
