65de85a0fc4cf81fcfed5dba5ba41f01889d51b6
Coderwelsch <4981006+Coderwelsch@users.noreply.github.com>
2024-08-05 13:13:02 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@2e84678ee289d23612b55d0b1bee92d6e2370a8e 🚀

---
+++ assets/panel.stories-B6qW_0zK.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-BJwPuJt0.js";import{P as l}from"./panel-DMNqUxst.js";import{B as e}from"./button-8880JDVt.js";import{g as d}from"./storybook-utils-C5xEhci0.js";import{T as u}from"./toggle-x7QpK8qU.js";import"./index-DX35FyXq.js";import"./class-names-B8CnD4Xb.js";import"./spinner-DVLNtNYV.js";import"./description-YAtjMrPw.js";import"./use-sync-refs-DTQ85WuU.js";import"./form-fields-xMSnkdfg.js";import"./index-CrrVXmAv.js";import"./hidden-BRuiewI3.js";import"./label-Bl9aPnpE.js";import"./use-resolve-button-type-DPkGNm3y.js";import"./keyboard-CMPngVPH.js";import"./bugs-DpEN4NTH.js";const E={title:"Panel",parameters:{...d("Simple container used to group and organize elements in the UI."),backgrounds:{default:"light"}},component:l,args:{className:"",children:"Panel with text content"}},o={},n=()=>{},r={args:{children:t.jsxs(t.Fragment,{children:[t.jsx(e,{variant:"primary",onClick:n,children:"Button A"}),t.jsx(u.Switch,{checked:!0,ariaLabel:"test",onChange:n}),t.jsx(e,{variant:"secondary",onClick:n,children:"Button B"}),t.jsx("p",{children:" Paragraph content"})]})}};var a,i,s;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(s=(i=o.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var p,c,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: <>
+                <Button variant="primary" onClick={noop}>
+                    Button A
+                </Button>
+                <Toggle.Switch checked ariaLabel="test" onChange={noop} />
+                <Button variant="secondary" onClick={noop}>
+                    Button B
+                </Button>
+                <p> Paragraph content</p>
+            </>
+  }
+}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const L=["Default","WithComponents"];export{o as Default,r as WithComponents,L as __namedExportsOrder,E as default};
