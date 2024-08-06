48784f1e66e3e51cd209a9fc332d604ee7033138
coderwelsch <joseph.ribbe@abusix.com>
2024-08-06 10:28:40 +0200

    added exports for interfaces

--- src/components/popover-menu/popover-menu-panel-group.tsx
+++ src/components/popover-menu/popover-menu-panel-group.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-interface PopoverMenuGroupProps {
+export interface PopoverMenuGroupProps {
     children: React.ReactNode;
 }
 
--- src/components/popover-menu/popover-menu-panel-title.tsx
+++ src/components/popover-menu/popover-menu-panel-title.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-interface PopoverMenuPanelTitleProps {
+export interface PopoverMenuPanelTitleProps {
     children: React.ReactNode;
 }
 
