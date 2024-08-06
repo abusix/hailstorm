56fa27c68f591193c5bc740cbc44c9c4dbecee5e
coderwelsch <joseph.ribbe@abusix.com>
2024-08-06 12:31:59 +0200

    added additional button component to make the pnael auto closable

--- src/components/popover-menu/popover-menu-panel-button.tsx
+++ src/components/popover-menu/popover-menu-panel-button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PopoverButton as HeadlessUiPopoverButton } from "@headlessui/react";
 import { classNames } from "../../util/class-names";
 
 const itemIntents = {
@@ -6,42 +7,39 @@ const itemIntents = {
     danger: "text-danger-500 fill-danger-500 hover:bg-danger-100",
 };
 
-const selectedItemIntents = {
+const activeItemIntents = {
     neutral: "bg-primary-100 fill-primary-400 text-primary-400 before:bg-primary-400",
     danger: "bg-danger-100 fill-danger-400 text-danger-500 before:bg-danger-400",
 };
 
-export interface PopoverMenuPanelItemProps {
+export interface PopoverMenuPanelButtonProps {
     children: React.ReactNode;
     onClick?: () => void;
     Icon?: React.ComponentType<{ className: string }>;
     variant?: keyof typeof itemIntents;
-    selected?: boolean;
+    active?: boolean;
 }
 
-export const PopoverMenuPanelItem = ({
+export const PopoverMenuPanelButton = ({
     children,
     onClick,
     Icon,
     variant = "neutral",
-    selected,
-}: PopoverMenuPanelItemProps) => {
+    active,
+}: PopoverMenuPanelButtonProps) => {
     return (
-        <div
+        <HeadlessUiPopoverButton
             className={classNames(
                 "relative flex w-full cursor-pointer flex-row items-center gap-3 overflow-hidden px-4 py-2 text-sm font-normal focus:ring-2 focus:ring-primary-200",
                 itemIntents[variant],
-                selected && selectedItemIntents[variant],
-                selected &&
+                active && activeItemIntents[variant],
+                active &&
                     "before:absolute before:left-0 before:top-0 before:h-full before:w-0.5 before:rounded-r-md"
             )}
             onClick={onClick}
-            role="menuitem"
-            onKeyDown={onClick}
-            tabIndex={0}
         >
             {Icon && <Icon className={classNames("h-3.5 w-3.5")} />}
             {children}
-        </div>
+        </HeadlessUiPopoverButton>
     );
 };
