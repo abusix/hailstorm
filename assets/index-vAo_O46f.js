c88e6490c290ff44774f1b86e9b6e90d48133050
Coderwelsch <4981006+Coderwelsch@users.noreply.github.com>
2024-08-15 12:03:18 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@51ef668d577d5468699e08f3e497d1d441beee36 🚀

---
+++ assets/index-vAo_O46f.js
@@ -0,0 +1,13 @@
+import{j as n}from"./jsx-runtime-BJwPuJt0.js";import{useMDXComponents as o}from"./index-D6p8fCaX.js";import{ae as s}from"./index-4IrknSup.js";import"./index-DX35FyXq.js";import"./iframe-CqXHEdhX.js";import"../sb-preview/runtime.js";import"./index-CrrVXmAv.js";import"./index-D-8MO0q_.js";import"./index-F9OOiXnC.js";import"./index-DrFu-skq.js";function i(t){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Home"}),`
+`,n.jsx(e.h1,{id:"hailstorm",children:"Hailstorm"}),`
+`,n.jsx(e.h2,{id:"introduction",children:"Introduction"}),`
+`,n.jsx(e.p,{children:"Hailstorm is a design system for Abusix. It is a collection of reusable components, guidelines, and best practices that can be used to build consistent user interfaces across all of our products."}),`
+`,n.jsx(e.h2,{id:"getting-started",children:"Getting Started"}),`
+`,n.jsx(e.h3,{id:"installation",children:"Installation"}),`
+`,n.jsx(e.p,{children:"Hailstorm is available as an npm package. To install it, run the following command in your project's root directory:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-bash",children:`npm install @abusix/hailstorm
+`})}),`
+`,n.jsx(e.h2,{id:"documentation",children:"Documentation"}),`
+`,n.jsx(e.p,{children:"The documentation for Hailstorm will be available soon."}),`
+`,n.jsx(e.h2,{id:"license",children:"License"}),`
+`,n.jsxs(e.p,{children:["Hailstorm is licensed under the ",n.jsx(e.a,{href:"LICENSE",children:"MIT License"}),"."]})]})}function j(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{j as default};
