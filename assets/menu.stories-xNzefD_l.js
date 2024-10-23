b0fb239d1c20945a2bc23824bd8b2de18401a8d5
mnlfischer <mnlfischer@users.noreply.github.com>
2024-10-23 13:06:25 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@aea239c5043bdc444ca6a4c48d231ffd68fd851c 🚀

---
+++ assets/menu.stories-xNzefD_l.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-C798UXYj.js";import{a as t}from"./menu-6dEOZWfD.js";import"./index-0d5eMwbi.js";import"./use-resolve-button-type-D_6M6Au0.js";import"./keyboard-C2Ifhsxt.js";import"./use-sync-refs-DqQDZvJ6.js";import"./index-BoyHNU0R.js";import"./use-text-value-ehRDfXkw.js";import"./floating-ERp4LevH.js";import"./use-inert-others-CzqV763I.js";import"./portal-CLj4HHuk.js";import"./focus-management-DaVlPEX1.js";import"./use-server-handoff-complete-JgOu4JLg.js";import"./open-closed-DC3t9YEZ.js";import"./bugs-DpEN4NTH.js";import"./description-D9Ee5mtv.js";import"./label-BBnH-iCi.js";import"./menu-info-item-CC1TeP-c.js";import"./menu-separator-yO-BIXMb.js";import"./menu-title-DpAy2147.js";const O={title:"Menu",component:t},n={render:()=>e.jsx("div",{className:"w-52",children:e.jsxs(t,{children:[e.jsx(t.Button,{children:"Open Menu"}),e.jsxs(t.Items,{children:[e.jsx(t.Title,{children:"TITLE"}),e.jsx(t.Item,{disabled:!1,children:"Label"}),e.jsx(t.Item,{children:"Label"}),e.jsx(t.Item,{children:"Label"})]})]})})};var r,m,i;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <div className="w-52">
+            <Menu>
+                <Menu.Button>Open Menu</Menu.Button>
+                <Menu.Items>
+                    <Menu.Title>TITLE</Menu.Title>
+                    <Menu.Item disabled={false}>Label</Menu.Item>
+                    <Menu.Item>Label</Menu.Item>
+                    <Menu.Item>Label</Menu.Item>
+                </Menu.Items>
+            </Menu>
+        </div>
+}`,...(i=(m=n.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const w=["Default"];export{n as Default,w as __namedExportsOrder,O as default};
