4d75723ff4fbbd02484bd98d275a600f93ff37da
mnlfischer <mnlfischer@users.noreply.github.com>
2024-10-23 09:00:28 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@521da3abbdd8ff6e9bee6485dd22742bc0c33984 🚀

---
+++ assets/menu-item.stories-C6z3CClY.js
@@ -0,0 +1,15 @@
+import{j as r}from"./jsx-runtime-C798UXYj.js";import{S as s}from"./zoom-to-fit-icon-DdsCB6de.js";import{M as n,a as i}from"./menu-BbZFQK8W.js";import"./index-0d5eMwbi.js";import"./use-resolve-button-type-D_6M6Au0.js";import"./keyboard-C2Ifhsxt.js";import"./use-sync-refs-DqQDZvJ6.js";import"./index-BoyHNU0R.js";import"./use-text-value-ehRDfXkw.js";import"./floating-ERp4LevH.js";import"./use-inert-others-CzqV763I.js";import"./portal-CLj4HHuk.js";import"./focus-management-DaVlPEX1.js";import"./use-server-handoff-complete-JgOu4JLg.js";import"./open-closed-DC3t9YEZ.js";import"./bugs-DpEN4NTH.js";import"./description-D9Ee5mtv.js";import"./label-BBnH-iCi.js";import"./menu-info-item-CC1TeP-c.js";import"./menu-separator-yO-BIXMb.js";import"./menu-title-DpAy2147.js";const N={title:"Menu/MenuItem",component:n},e={argTypes:{disabled:{type:"boolean",defaultValue:!1}},render:m=>r.jsx("div",{className:"w-52",children:r.jsx(i,{children:r.jsx(n,{LeftIcon:s,...m,children:"Label"})})})};var t,o,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  argTypes: {
+    disabled: {
+      type: "boolean",
+      defaultValue: false
+    }
+  },
+  render: args => <div className="w-52">
+            <Menu>
+                <MenuItem LeftIcon={ChatIcon} {...args}>
+                    Label
+                </MenuItem>
+            </Menu>
+        </div>
+}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,N as default};
