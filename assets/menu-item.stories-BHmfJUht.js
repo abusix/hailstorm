fd36b57605d947465cea30a2fb10943c4eea66f7
mnlfischer <mnlfischer@users.noreply.github.com>
2024-10-28 08:21:47 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@0ec4e5a18e7bd0dcdbaae68a01973754b00513a4 🚀

---
+++ assets/menu-item.stories-BHmfJUht.js
@@ -0,0 +1,15 @@
+import{j as r}from"./jsx-runtime-C798UXYj.js";import{S as s}from"./zoom-to-fit-icon-DdsCB6de.js";import{M as n,a as i}from"./menu-CrBWIwE1.js";import"./index-0d5eMwbi.js";import"./use-resolve-button-type-HzzQKYtS.js";import"./keyboard-D0BTZntQ.js";import"./use-sync-refs-DhZgiOVe.js";import"./index-BoyHNU0R.js";import"./use-text-value-D3ZnWpLS.js";import"./floating-Bhi-vCn9.js";import"./use-inert-others-D4nVU6H_.js";import"./portal-BQQr4JoD.js";import"./focus-management-CKkqTXfi.js";import"./use-server-handoff-complete-UouYGEcd.js";import"./open-closed-FM4Rfwkx.js";import"./bugs-DpEN4NTH.js";import"./description-CaDbXm72.js";import"./label-fWs8-lsP.js";import"./menu-info-item-CC1TeP-c.js";import"./menu-separator-yO-BIXMb.js";import"./menu-title-DpAy2147.js";const N={title:"Menu/MenuItem",component:n},e={argTypes:{disabled:{type:"boolean",defaultValue:!1}},render:m=>r.jsx("div",{className:"w-52",children:r.jsx(i,{children:r.jsx(n,{LeftIcon:s,...m,children:"Label"})})})};var t,o,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  argTypes: {
+    disabled: {
+      type: "boolean",
+      defaultValue: false
+    }
+  },
+  render: args => <div className="w-52">
+            <Menu>
+                <MenuItem LeftIcon={ChatIcon} {...args}>
+                    Label
+                </MenuItem>
+            </Menu>
+        </div>
+}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,N as default};
---
+++ assets/use-is-mounted-DN0doZLi.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-0d5eMwbi.js";import{n as e}from"./use-sync-refs-DhZgiOVe.js";function f(){let r=t.useRef(!1);return e(()=>(r.current=!0,()=>{r.current=!1}),[]),r}export{f};
