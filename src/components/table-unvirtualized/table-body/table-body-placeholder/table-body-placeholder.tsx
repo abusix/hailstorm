a8a6baaafa4862f54116cd91a22b4c2b8f030f8d
coderwelsch <joseph.ribbe@abusix.com>
2024-11-15 15:24:47 +0100

    fixed formats

--- src/components/table-unvirtualized/table-body/table-body-placeholder/table-body-placeholder.tsx
+++ src/components/table-unvirtualized/table-body/table-body-placeholder/table-body-placeholder.tsx
@@ -18,7 +18,7 @@ export const TableEmptyPlaceholder = ({
     return (
         <TableBodyRow aria-label="No data available in this table">
             <TableBodyCell colSpan={colSpan} className="h-80">
-                <div className="flex min-h-96 flex-col items-center justify-center">
+                <div className="min-h-96 flex flex-col items-center justify-center">
                     <h5 className="headline-300 mb-1">{title}</h5>
 
                     <p className="paragraph-100 mb-2">{description}</p>
