b905325d18ee53267579114217956810add2f8fd
Pablo Allendes <allendes91@gmail.com>
2025-12-29 16:44:10 +0100

    feat: migrate SidebarMenuLink to semantic tokens

--- src/components/sidebar/sidebar-menu/sidebar-menu-link/sidebar-menu-link.tsx
+++ src/components/sidebar/sidebar-menu/sidebar-menu-link/sidebar-menu-link.tsx
@@ -10,7 +10,7 @@ export const SidebarMenuLink = ({ isActive, children }: SidebarMenuLinkProps) =>
     return (
         <div
             className={classNames(
-                "relative px-4 py-3 font-sans text-sm hover:bg-neutral-200 active:bg-neutral-100",
+                "relative px-4 py-3 font-sans text-sm text-text-base hover:bg-bg-muted active:bg-bg-subtle",
                 isActive &&
                     "bg-primary-100 text-primary-400 before:bg-primary-400 hover:bg-primary-100 before:absolute before:top-0 before:bottom-0 before:left-0 before:block before:w-[3px] before:rounded-r-md"
             )}
