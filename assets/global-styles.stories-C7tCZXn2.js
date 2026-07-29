6f08dda9a18f0541c9945e38e2529f54dfb8ddb6
pallendes <allendes91@gmail.com>
2026-07-29 07:29:09 +0000

    Deploying to gh-pages-storybook from @ abusix/hailstorm@1be825955c218110683d4dfcb36cf2ddf0ba6019 🚀

---
+++ assets/global-styles.stories-C7tCZXn2.js
@@ -0,0 +1,175 @@
+import{j as e}from"./jsx-runtime-DiklIkkE.js";import{T as l}from"./table-unvirtualized-Bpp0Q1pp.js";import"./index-DRjF_FHU.js";import"./class-names-AhVFtEzo.js";import"./spinner-DQmhNHpC.js";const z={title:"Global CSS Style Classes"},a={render:()=>e.jsxs("div",{className:"flex flex-col gap-4",children:[e.jsx("h3",{className:"headline-700",children:"Headline Styles"}),e.jsxs(l,{children:[e.jsx(l.Header,{children:e.jsxs(l.Header.Row,{children:[e.jsx(l.Header.Cell,{children:"ClassName"}),e.jsx(l.Header.Cell,{children:"Example"})]})}),e.jsxs(l.Body,{children:[e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-100"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-100",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-200"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-200",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-300"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-300",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-400"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-400",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-500"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-500",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-600"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-600",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-700"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-700",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-800"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-800",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"headline-900"})}),e.jsx(l.Body.Cell,{children:e.jsx("h1",{className:"headline-900",children:"The quick brown fox jumps over the lazy dog"})})]})]})]})]})},n={render:()=>e.jsxs("div",{className:"flex flex-col gap-4",children:[e.jsx("h3",{className:"headline-700",children:"Paragraph Styles"}),e.jsxs(l,{children:[e.jsx(l.Header,{children:e.jsxs(l.Header.Row,{children:[e.jsx(l.Header.Cell,{children:"ClassName"}),e.jsx(l.Header.Cell,{children:"Example"})]})}),e.jsxs(l.Body,{children:[e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"paragraph-100"})}),e.jsx(l.Body.Cell,{children:e.jsx("p",{className:"paragraph-100",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"paragraph-200"})}),e.jsx(l.Body.Cell,{children:e.jsx("p",{className:"paragraph-200",children:"The quick brown fox jumps over the lazy dog"})})]}),e.jsxs(l.Body.Row,{children:[e.jsx(l.Body.Cell,{children:e.jsx("pre",{children:"paragraph-300"})}),e.jsx(l.Body.Cell,{children:e.jsx("p",{className:"paragraph-300",children:"The quick brown fox jumps over the lazy dog"})})]})]})]})]})},d={render:()=>e.jsxs("div",{className:"flex flex-col gap-4",children:[e.jsx("h3",{className:"headline-700 font-mono",children:"inline-link"}),e.jsxs("p",{className:"paragraph-200",children:[e.jsx("a",{href:"https://abusix.com",className:"inline-link",children:"Be or not to be,"})," ","that is the question: Whether ’tis nobler in the mind to suffer The slings and arrows of outrageous fortune, …"]})]})};var r,i,o;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-4">
+            <h3 className="headline-700">Headline Styles</h3>
+
+            <TableUnvirtualized>
+                <TableUnvirtualized.Header>
+                    <TableUnvirtualized.Header.Row>
+                        <TableUnvirtualized.Header.Cell>ClassName</TableUnvirtualized.Header.Cell>
+                        <TableUnvirtualized.Header.Cell>Example</TableUnvirtualized.Header.Cell>
+                    </TableUnvirtualized.Header.Row>
+                </TableUnvirtualized.Header>
+
+                <TableUnvirtualized.Body>
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-100</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-100">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-200</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-200">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-300</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-300">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-400</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-400">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-500</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-500">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-600</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-600">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-700</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-700">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-800</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-800">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>headline-900</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <h1 className="headline-900">
+                                The quick brown fox jumps over the lazy dog
+                            </h1>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+                </TableUnvirtualized.Body>
+            </TableUnvirtualized>
+        </div>
+}`,...(o=(i=a.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};var s,h,t;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-4">
+            <h3 className="headline-700">Paragraph Styles</h3>
+
+            <TableUnvirtualized>
+                <TableUnvirtualized.Header>
+                    <TableUnvirtualized.Header.Row>
+                        <TableUnvirtualized.Header.Cell>ClassName</TableUnvirtualized.Header.Cell>
+                        <TableUnvirtualized.Header.Cell>Example</TableUnvirtualized.Header.Cell>
+                    </TableUnvirtualized.Header.Row>
+                </TableUnvirtualized.Header>
+
+                <TableUnvirtualized.Body>
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>paragraph-100</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <p className="paragraph-100">
+                                The quick brown fox jumps over the lazy dog
+                            </p>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>paragraph-200</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <p className="paragraph-200">
+                                The quick brown fox jumps over the lazy dog
+                            </p>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+
+                    <TableUnvirtualized.Body.Row>
+                        <TableUnvirtualized.Body.Cell>
+                            <pre>paragraph-300</pre>
+                        </TableUnvirtualized.Body.Cell>
+                        <TableUnvirtualized.Body.Cell>
+                            <p className="paragraph-300">
+                                The quick brown fox jumps over the lazy dog
+                            </p>
+                        </TableUnvirtualized.Body.Cell>
+                    </TableUnvirtualized.Body.Row>
+                </TableUnvirtualized.Body>
+            </TableUnvirtualized>
+        </div>
+}`,...(t=(h=n.parameters)==null?void 0:h.docs)==null?void 0:t.source}}};var c,u,y;d.parameters={...d.parameters,docs:{...(c=d.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-4">
+            <h3 className="headline-700 font-mono">inline-link</h3>
+
+            <p className="paragraph-200">
+                <a href="https://abusix.com" className="inline-link">
+                    Be or not to be,
+                </a>{" "}
+                that is the question: Whether ’tis nobler in the mind to suffer The slings and
+                arrows of outrageous fortune, …
+            </p>
+        </div>
+}`,...(y=(u=d.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};const B=["Headlines","Paragraphs","InlineLink"];export{a as Headlines,d as InlineLink,n as Paragraphs,B as __namedExportsOrder,z as default};
